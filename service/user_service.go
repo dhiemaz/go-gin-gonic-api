35bc3909e8f8fd66ec4c8401ea731c6c9f401fff
hendisantika <hendisantika@yahoo.co.id>
2024-08-20 06:49:02 +0700

    Add UpdateUserData 🫸🌀✏️📗🐧🐳

--- service/user_service.go
+++ service/user_service.go
@@ -1,6 +1,14 @@
 package service
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	log "github.com/sirupsen/logrus"
+	"go-gin-gonic-api/constant"
+	"go-gin-gonic-api/domain/dao"
+	"go-gin-gonic-api/pkg"
+	"net/http"
+	"strconv"
+)
 
 type UserService interface {
 	GetAllUser(c *gin.Context)
@@ -12,4 +20,36 @@ type UserService interface {
 
 type UserServiceImpl struct {
 	userRepository repository.UserRepository
+}
+
+func (u UserServiceImpl) UpdateUserData(c *gin.Context) {
+	defer pkg.PanicHandler(c)
+
+	log.Info("start to execute program update user data by id")
+	userID, _ := strconv.Atoi(c.Param("userID"))
+
+	var request dao.User
+	if err := c.ShouldBindJSON(&request); err != nil {
+		log.Error("Happened error when mapping request from FE. Error", err)
+		pkg.PanicException(constant.InvalidRequest)
+	}
+
+	data, err := u.userRepository.FindUserById(userID)
+	if err != nil {
+		log.Error("Happened error when get data from database. Error", err)
+		pkg.PanicException(constant.DataNotFound)
+	}
+
+	data.RoleID = request.RoleID
+	data.Email = request.Email
+	data.Name = request.Password
+	data.Status = request.Status
+	u.userRepository.Save(&data)
+
+	if err != nil {
+		log.Error("Happened error when updating data to database. Error", err)
+		pkg.PanicException(constant.UnknownError)
+	}
+
+	c.JSON(http.StatusOK, pkg.BuildResponse(constant.Success, data))
 }
