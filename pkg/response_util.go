f207438e86f6409e13f094542f2ee10334d6ae79
hendisantika <hendisantika@yahoo.co.id>
2024-08-20 06:43:44 +0700

    Add BuildResponse_ package 🫸🌀✏️📗🐧🐳

--- pkg/response_util.go
+++ pkg/response_util.go
@@ -12,3 +12,11 @@ func Null() interface{} {
 func BuildResponse[T any](responseStatus constant.ResponseStatus, data T) dto.ApiResponse[T] {
 	return BuildResponse_(responseStatus.GetResponseStatus(), responseStatus.GetResponseMessage(), data)
 }
+
+func BuildResponse_[T any](status string, message string, data T) dto.ApiResponse[T] {
+	return dto.ApiResponse[T]{
+		ResponseKey:     status,
+		ResponseMessage: message,
+		Data:            data,
+	}
+}
