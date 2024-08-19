08d244d86a6a82989dcfa1b93bc4f73b188bb867
hendisantika <hendisantika@yahoo.co.id>
2024-08-20 06:37:13 +0700

    Add GetResponseMessage 🫸🌀✏️📗🐧🐳

--- constant/app_constant.go
+++ constant/app_constant.go
@@ -16,3 +16,7 @@ const (
 func (r ResponseStatus) GetResponseStatus() string {
 	return [...]string{"SUCCESS", "DATA_NOT_FOUND", "UNKNOWN_ERROR", "INVALID_REQUEST", "UNAUTHORIZED"}[r-1]
 }
+
+func (r ResponseStatus) GetResponseMessage() string {
+	return [...]string{"Success", "Data Not Found", "Unknown Error", "Invalid Request", "Unauthorized"}[r-1]
+}
