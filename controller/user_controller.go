5c206378706185c0c85364fc93666eb8344790f4
hendisantika <hendisantika@yahoo.co.id>
2024-08-20 06:54:20 +0700

    Add UserControllerImpl 🫸🌀✏️📗🐧🐳

--- controller/user_controller.go
+++ controller/user_controller.go
@@ -1,6 +1,9 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"go-gin-gonic-api/service"
+)
 
 type UserController interface {
 	GetAllUserData(c *gin.Context)
@@ -8,4 +11,8 @@ type UserController interface {
 	GetUserById(c *gin.Context)
 	UpdateUserData(c *gin.Context)
 	DeleteUser(c *gin.Context)
+}
+
+type UserControllerImpl struct {
+	svc service.UserService
 }
