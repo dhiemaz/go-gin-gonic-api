7e05140f042cf52fc351c440716e9ef9daa644c5
hendisantika <hendisantika@yahoo.co.id>
2024-08-20 06:35:14 +0700

    Add getLoggerLevel function 🫸🌀✏️📗🐧🐳

--- config/logger.go
+++ config/logger.go
@@ -16,5 +16,15 @@ func InitLog() {
 		ShowFullLevel:   true,
 		CallerFirst:     true,
 	})
+}
 
+func getLoggerLevel(value string) log.Level {
+	switch value {
+	case "DEBUG":
+		return log.DebugLevel
+	case "TRACE":
+		return log.TraceLevel
+	default:
+		return log.InfoLevel
+	}
 }
