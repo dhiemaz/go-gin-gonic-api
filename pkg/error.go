b1c25740be4135586618e074263a9c7d76f50bdb
hendisantika <hendisantika@yahoo.co.id>
2024-08-20 06:40:28 +0700

    Add PanicException_ package 🫸🌀✏️📗🐧🐳

---
+++ pkg/error.go
@@ -0,0 +1,14 @@
+package pkg
+
+import (
+	"errors"
+	"fmt"
+)
+
+func PanicException_(key string, message string) {
+	err := errors.New(message)
+	err = fmt.Errorf("%s: %w", key, err)
+	if err != nil {
+		panic(err)
+	}
+}
