429856183fd3e11ad222ba7fc161228bbf78c4aa
hendisantika <hendisantika@yahoo.co.id>
2024-08-20 06:47:52 +0700

    Add userCtrlSet wire config 🫸🌀✏️📗🐧🐳

--- config/wire_gen.go
+++ config/wire_gen.go
@@ -19,3 +19,5 @@ var db = wire.NewSet(ConnectToDB)
 var userServiceSet = wire.NewSet(service.UserServiceInit, wire.Bind(new(service.UserService), new(*service.UserServiceImpl)))
 
 var userRepoSet = wire.NewSet(repository.UserRepositoryInit, wire.Bind(new(repository.UserRepository), new(*repository.UserRepositoryImpl)))
+
+var userCtrlSet = wire.NewSet(controller.UserControllerInit, wire.Bind(new(controller.UserController), new(*controller.UserControllerImpl)))
