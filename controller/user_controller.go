beef938ebc25bea81aae38bb3d698e12d3b35409
hendisantika <hendisantika@yahoo.co.id>
2024-08-20 06:54:07 +0700

    Add UserController 🫸🌀✏️📗🐧🐳

---
+++ controller/user_controller.go
@@ -0,0 +1,11 @@
+package controller
+
+import "github.com/gin-gonic/gin"
+
+type UserController interface {
+	GetAllUserData(c *gin.Context)
+	AddUserData(c *gin.Context)
+	GetUserById(c *gin.Context)
+	UpdateUserData(c *gin.Context)
+	DeleteUser(c *gin.Context)
+}
