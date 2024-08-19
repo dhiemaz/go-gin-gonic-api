0a3e509c71f9e1a76392cf7351d18a1da4612827
hendisantika <hendisantika@yahoo.co.id>
2024-08-20 06:46:19 +0700

    Add userRepoSet injector config 🫸🌀✏️📗🐧🐳

--- config/injector.go
+++ config/injector.go
@@ -5,3 +5,7 @@ var db = wire.NewSet(ConnectToDB)
 var userServiceSet = wire.NewSet(service.UserServiceInit,
 	wire.Bind(new(service.UserService), new(*service.UserServiceImpl)),
 )
+
+var userRepoSet = wire.NewSet(repository.UserRepositoryInit,
+	wire.Bind(new(repository.UserRepository), new(*repository.UserRepositoryImpl)),
+)
