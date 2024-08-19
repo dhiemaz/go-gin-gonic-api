ee64eb28840d0e1fc6204ea8a6e9481857c3f5e6
hendisantika <hendisantika@yahoo.co.id>
2024-08-20 06:43:26 +0700

    Add Null package 🫸🌀✏️📗🐧🐳

--- pkg/response_util.go
+++ pkg/response_util.go
@@ -1,5 +1,14 @@
 package pkg
 
+import (
+	"go-gin-gonic-api/constant"
+	"go-gin-gonic-api/domain/dto"
+)
+
 func Null() interface{} {
 	return nil
+}
+
+func BuildResponse[T any](responseStatus constant.ResponseStatus, data T) dto.ApiResponse[T] {
+	return BuildResponse_(responseStatus.GetResponseStatus(), responseStatus.GetResponseMessage(), data)
 }
