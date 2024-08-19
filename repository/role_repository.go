b04ce3b99848bc7d5b607126f5e88b820ae67e2e
hendisantika <hendisantika@yahoo.co.id>
2024-08-20 06:51:02 +0700

    Add RoleRepositoryInit 🫸🌀✏️📗🐧🐳

--- repository/role_repository.go
+++ repository/role_repository.go
@@ -1,6 +1,9 @@
 package repository
 
-import "gorm.io/gorm"
+import (
+	"go-gin-gonic-api/domain/dao"
+	"gorm.io/gorm"
+)
 
 type RoleRepository interface {
 	FindAllRole()
@@ -12,4 +15,11 @@ type RoleRepositoryImpl struct {
 
 func (r RoleRepositoryImpl) FindAllRole() {
 	panic("implement me")
+}
+
+func RoleRepositoryInit(db *gorm.DB) *RoleRepositoryImpl {
+	db.AutoMigrate(&dao.Role{}, &dao.User{})
+	return &RoleRepositoryImpl{
+		db: db,
+	}
 }
