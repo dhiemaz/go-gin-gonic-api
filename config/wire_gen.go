2080b9479a86da920aaaf806f7caa98229911cec
hendisantika <hendisantika@yahoo.co.id>
2024-08-20 06:48:03 +0700

    Add roleRepoSet wire config 🫸🌀✏️📗🐧🐳

--- config/wire_gen.go
+++ config/wire_gen.go
@@ -21,3 +21,5 @@ var userServiceSet = wire.NewSet(service.UserServiceInit, wire.Bind(new(service.
 var userRepoSet = wire.NewSet(repository.UserRepositoryInit, wire.Bind(new(repository.UserRepository), new(*repository.UserRepositoryImpl)))
 
 var userCtrlSet = wire.NewSet(controller.UserControllerInit, wire.Bind(new(controller.UserController), new(*controller.UserControllerImpl)))
+
+var roleRepoSet = wire.NewSet(repository.RoleRepositoryInit, wire.Bind(new(repository.RoleRepository), new(*repository.RoleRepositoryImpl)))
