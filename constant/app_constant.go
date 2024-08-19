8b03ed845c416f48ec81d31190af999de6718f33
hendisantika <hendisantika@yahoo.co.id>
2024-08-20 06:36:42 +0700

    Add Constant Api 🫸🌀✏️📗🐧🐳

---
+++ constant/app_constant.go
@@ -0,0 +1,14 @@
+package constant
+
+type ResponseStatus int
+type Headers int
+type General int
+
+// Constant Api
+const (
+	Success ResponseStatus = iota + 1
+	DataNotFound
+	UnknownError
+	InvalidRequest
+	Unauthorized
+)
